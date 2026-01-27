41a2a3cc6e4a5ea81ca77f6b50a6482a3308dd3f
EllisSK <ellissinclairkent@gmail.com>
2026-01-27 17:59:36 +0000

    Added simple example test

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,3 +3,14 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 import pytest
+from src.analysis.models import *
+
+def test_sluice_model():
+    flow = 0.01
+    head = 0.00137021797
+    area = 0.1
+    Cd = 0.61
+
+    result = sluice_equation(head, area, Cd)
+
+    assert float(result) == pytest.approx(flow)
